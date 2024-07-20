ea57d35f6cebc625eb654aea349d9a6f412247ef
mayhernandezcollao <121122933+mayhernandezcollao@users.noreply.github.com>
2024-07-20 11:12:20 -0400

    Animación del menu para dispositivos pequeños

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer className="flex w-full justify-center bg-black z-20">
-            <div className="w-full border-t-[1px] border-t-gray-500 bg-black max-w-6xl px-5">
+            <div className="w-full border-t-[1px] border-t-gray-600 bg-black max-w-6xl px-5">
                 <p className="text-center py-5 text-[10px]">&copy; 2024 Maryorie Hernandez Collao. Todos los derechos reservados.</p>
             </div>
         </footer>
