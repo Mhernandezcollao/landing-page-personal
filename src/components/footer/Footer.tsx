4883ef08cf47a9bce1761d51edc087200e1004fc
mayhernandezcollao <121122933+mayhernandezcollao@users.noreply.github.com>
2024-07-20 11:53:47 -0400

    detalles footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div className='flex z-30 bg-black'>
-            <div className="w-full border-t-[1px] border-t-gray-600 px-5 bg-black z-30">
+        <footer className='flex z-50 bg-black'>
+            <div className="w-full border-t-[1px] border-t-gray-600 px-5 bg-black z-50">
                 <p className="text-center py-5 text-[10px]">&copy; 2024 Maryorie Hernandez Collao. Todos los derechos reservados.</p>
             </div>
-        </div>
+        </footer>
     )
 }
