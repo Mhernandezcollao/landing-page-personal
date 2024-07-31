a95b7b15e987a491e346410a02506c7e153ada96
mayhernandezcollao <121122933+mayhernandezcollao@users.noreply.github.com>
2024-07-31 08:59:18 -0400

    Modificacion de diseño footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer>
-            <div className='relative w-full z-50 bg-black'>
-                <div className="absolute w-full border-t-[1px] border-t-gray-600 px-5 bg-black z-50">
-                    <p className="text-center py-5 text-[10px]">&copy; 2024 Maryorie Hernandez Collao. Todos los derechos reservados.</p>
-                </div>
+        <footer className='flex z-50 bg-black'>
+            <div className="w-full border-t-[1px] border-t-gray-600 px-5 bg-black z-50">
+                <p className="text-center py-5 text-[10px]">&copy; 2024 Maryorie Hernandez Collao. Todos los derechos reservados.</p>
             </div>
         </footer>
     )
